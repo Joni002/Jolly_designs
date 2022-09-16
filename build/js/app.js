c8d65b5e4be285e7ead52798be2df4b1f6e2d0a6
Sirman Artem <sirman.artem000@gmail.com>
2022-09-16 18:14:40 +0300

    Add subheader and drop list

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,21 @@
-'use strict';
-
-$(document).ready(function () {});
+$(document).ready(function () {
+  $('.sudheader_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: "<img src='img/svg/icons/icon-left.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='img/svg/icons/icon-right.svg' class='next' alt='2'>",
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
+});
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,25 @@
-'use strict'
 $( document ).ready(
 	function() {
+		$('.sudheader_slider').slick({
+			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			prevArrow: "<img src='img/svg/icons/icon-left.svg' class='prev' alt='1'>",
+    		nextArrow: "<img src='img/svg/icons/icon-right.svg' class='next' alt='2'>",
+			responsive:[
+				{
+					breakpoint: 1200,
+					settings: {
+						slidesToShow:2
+					}
+				},
+				{
+					breakpoint: 750,
+					settings: {
+						slidesToShow:1
+					}
+				}
+				]
+		  });
 	}
 );
